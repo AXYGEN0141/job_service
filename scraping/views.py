79ef2161047ff1bea2247ad2247f09270bbcda36
AXYGEN01141 <zombe@yandex.ru>
2020-12-01 22:01:18 +0300

    Home html page is created

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from scraping.models import City, Language, Vacancy
 
 # Create your views here.
+
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'scraping/home.html', {'object_list': qs})
